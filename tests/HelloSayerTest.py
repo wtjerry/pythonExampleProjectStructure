fd99d5b87543a7d298ffe95dc3611e87ab921f53
wtjerry <wtjerry123@gmail.com>
2017-11-15 20:11:51 +0100

    changed single quotation marks to double where appropriate.

--- tests/HelloSayerTest.py
+++ tests/HelloSayerTest.py
@@ -4,7 +4,7 @@ from pythonExampleProjectStructure.HelloSayer import HelloSayer
 
 class HelloSayerTest(unittest.TestCase):
     def test_say_hello(self):
-        self.assertEqual('hello Freddy', HelloSayer('Freddy').create_hello_message())
+        self.assertEqual("hello Freddy", HelloSayer("Freddy").create_hello_message())
 
 
 if __name__ == '__main__':
